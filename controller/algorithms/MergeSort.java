5a036b4647216e2ec00f39e01e9ba16817a310e9
Natasha <155433222+Natashaa7@users.noreply.github.com>
2025-01-20 09:41:21 +0545

    Update MergeSort.java

--- controller/algorithms/MergeSort.java
+++ controller/algorithms/MergeSort.java
@@ -11,16 +11,35 @@ import java.util.List;
 /**
  *
  * LMUID: 23048637
+ *
  * @author Natasha Babu
  */
 public class MergeSort {
-    // MergeSort Algortihm
+
+    /**
+     * Sorts a list of JewelleryModel objects by their total cost in ascending or descending order.
+     * This method uses the Merge Sort algorithm for sorting.
+     *
+     * @param unsortedData the list of JewelleryModel objects to be sorted
+     * @param isAsc a boolean flag indicating the sort order: - true for
+     * ascending order - false for descending order
+     * @return an ArrayList of JewelleryModel objects sorted by total cost in the specified order
+     */
     public ArrayList<JewelleryModel> sortByTotalCost(List<JewelleryModel> unsortedData, boolean isAsc) {
         ArrayList<JewelleryModel> dataToSort = new ArrayList(unsortedData);
         mergeSort(dataToSort, 0, dataToSort.size() - 1, isAsc);
         return dataToSort;
     }
-    
+
+    /**
+     * Recursively sorts a portion of the list using the Merge Sort algorithm.
+     *
+     * @param list the ArrayList of JewelleryModel objects to be sorted
+     * @param first the starting index of the portion of the list to sort
+     * @param second the ending index of the portion of the list to sort
+     * @param isAsc a boolean flag indicating the sort order: - true for
+     * ascending order - false for descending order
+     */
     private void mergeSort(ArrayList<JewelleryModel> list, int first, int second, boolean isAsc) {
         if (first >= second) {
             return;
@@ -35,7 +54,18 @@ public class MergeSort {
         merge(list, first, mid, second, isAsc);
     }
 
-    // Method for merging data
+    /**
+     * Merges two sorted halves of the list into a single sorted list based on the total cost of the JewelleryModel objects. 
+     * This method is used by the mergeSort algorithm to combine two sorted sublists into a fully sorted list.
+     *
+     * @param list the ArrayList of JewelleryModel objects to be sorted
+     * @param left the starting index of the first half of the list
+     * @param mid the middle index where the list is divided
+     * @param right the ending index of the second half of the list
+     * @param isAsc a boolean flag indicating the sort order: 
+     * - true for ascending order 
+     * - false for descending order
+     */
     private void merge(ArrayList<JewelleryModel> list, int left, int mid, int right, boolean isAsc) {
         ArrayList<JewelleryModel> temp = new ArrayList();
         int firstCounter = left, secondCounter = mid + 1;
@@ -69,5 +99,5 @@ public class MergeSort {
             list.set(left + k, temp.get(k));
         }
     }
-    
+
 }
