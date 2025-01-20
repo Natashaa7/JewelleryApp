278717d1d4cb31e334975b50f067bd8735c9e9de
Natasha <155433222+Natashaa7@users.noreply.github.com>
2025-01-20 09:40:57 +0545

    Update InsertionSort.java

--- controller/algorithms/InsertionSort.java
+++ controller/algorithms/InsertionSort.java
@@ -11,9 +11,21 @@ import java.util.List;
 /**
  *
  * LMUID: 23048637
+ *
  * @author Natasha Babu
  */
 public class InsertionSort {
+
+    /**
+     * Sorts a list of JewelleryModel objects by their code in ascending or descending order. 
+     * This method uses the Insertion Sort algorithm for sorting.
+     *
+     * @param unsortedData the list of JewelleryModel objects to be sorted
+     * @param isAsc a boolean flag indicating the sort order: 
+     * - true for ascending order 
+     * - false for descending order
+     * @return an ArrayList of JewelleryModel objects sorted by code in the specified order
+     */
     public ArrayList<JewelleryModel> sortByCode(List<JewelleryModel> unsortedData, boolean isAsc) {
         ArrayList<JewelleryModel> dataToSort = new ArrayList(unsortedData);
 
@@ -32,5 +44,5 @@ public class InsertionSort {
             dataToSort.set(j + 1, keyItem);
         }
         return dataToSort;
-    }   
+    }
 }
